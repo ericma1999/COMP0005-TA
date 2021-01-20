76bf3c347a9e4b70a1f99a55457370e846d36dec
sudoPom42 <zcabofe@ucl.ac.uk>
2021-01-20 10:48:05 +0000

    added searhc function for bitonic arrays

--- week2/exercise3.py
+++ week2/exercise3.py
@@ -1,3 +1,4 @@
+from math import ceil
 def checkBitonic(numbers):
 
     reverse = list(reversed(numbers))
@@ -11,12 +12,12 @@ def checkBitonic(numbers):
         if (numbers[i] > numbers[i + 1]):
             pos = i + 1
             break
-        
+
         if(reverse[i] > reverse[i+1]):
             pos = len(numbers) - (i + 1)
             break
 
-    
+
     first_half = numbers[0:pos]
     second_half = numbers[pos:]
 
@@ -25,9 +26,22 @@ def checkBitonic(numbers):
         return False
 
     if list(reversed(second_half)) == sorted(second_half) and (first_half == sorted(first_half)):
-        return True    
+        return True
 
-
+def findMaxBitonic(values):
+    top = len(values) - 1
+    bot = 0
+    while(True):
+        mid = ceil((top+bot)/2)
+        val = values[mid]
+        left = values[mid-1]
+        right = values[mid+1]
+        if(val >= left and val >= right):
+            return values[mid]
+        elif(val >= left and val <= right):
+            bot = mid
+        else:
+            top = mid
 
 print(checkBitonic([1, 3, 4, 6, 9, 14, 11, 7, 2, -4, -9] ))
 
