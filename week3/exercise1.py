b98bdd96ebcb9f9709c794da053e4768c36ca67e
sudoPom42 <zcabofe@ucl.ac.uk>
2021-01-25 13:18:51 +0000

    Completed exercise 1

--- week3/exercise1.py
+++ week3/exercise1.py
@@ -16,4 +16,74 @@ def sortArray(vals):
         newList.append(sort(word))
     return newList
 
-print(sortArray(words))
+
+def mergeSort(arr,arr2):
+    if len(arr) > 1:
+
+         # Finding the mid of the array
+        mid = len(arr)//2
+
+        # Dividing the array elements
+        L = arr[:mid]
+        L2 = arr2[:mid]
+
+        # into 2 halves
+        R = arr[mid:]
+        R2 = arr2[mid:]
+
+        # Sorting the first half
+        mergeSort(L,L2)
+
+
+        # Sorting the second half
+        mergeSort(R,R2)
+
+        i = j = k = 0
+
+        # Copy data to temp arrays L[] and R[]
+        while i < len(L) and j < len(R):
+            if L[i] < R[j]:
+                arr[k] = L[i]
+                arr2[k] =L2[i]
+                i += 1
+            else:
+                arr[k] = R[j]
+                arr2[k] = R2[j]
+                j += 1
+            k += 1
+
+        # Checking if any element was left
+        while i < len(L):
+            arr[k] = L[i]
+            arr2[k] = L2[i]
+            i += 1
+            k += 1
+
+        while j < len(R):
+            arr[k] = R[j]
+            arr2[k] = R2[j]
+            j += 1
+            k += 1
+
+def anagrams(words, words2):
+    groups = [[]]
+    count = 0
+    value = words.pop(0)
+    groups[count].append(words2.pop(0))
+    while(len(words) > 0):
+        while(value == words[0]):
+            words.pop(0)
+            groups[count].append(words2.pop(0))
+        count += 1
+        value = words.pop(0)
+        groups.append([])
+        groups[count].append(words2.pop(0))
+    return groups
+
+sortedWords = sortArray(words)
+mergeSort(sortedWords,words)
+
+print(sortedWords)
+print(words)
+
+print(anagrams(sortedWords, words))
