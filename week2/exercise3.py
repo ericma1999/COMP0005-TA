62c8b8629b4193e0b3bba0b4403af72aa27f016d
Passawis <official.passawis@gmail.com>
2021-01-25 17:56:05 +0700

    added exercise3

--- week2/exercise3.py
+++ week2/exercise3.py
@@ -1,4 +1,30 @@
-def binarySearch(values):
+def binSearch(array, element):
+    mid = 0
+    start = 0
+    end = len(array)
+    step = 0
+
+    while (start <= end):
+        mid = (start + end) // 2
+        
+        
+        step = step+1
+        if element == array[mid]:
+            return mid
+
+
+
+        if element < array[mid]:
+            end = mid - 1
+
+
+        else:
+            start = mid + 1
+    return -1
+
+
+
+def highestValuePos(values):
     top = len(values) - 1
     mid = (len(values) - 1) // 2
     bottom = 0
@@ -19,7 +45,7 @@ def binarySearch(values):
             mid = (bottom + mid) // 2
 
 
-def checkBitonic(numbers):
+def checkBitonic(numbers, value):
 
     reverse = list(reversed(numbers))
     pos = 0
@@ -27,7 +53,7 @@ def checkBitonic(numbers):
     if (len(numbers) == 1) or not numbers:
         return False
 
-    pos = binarySearch(numbers)
+    pos = highestValuePos(numbers)
 
 
     # increasing part
@@ -39,14 +65,28 @@ def checkBitonic(numbers):
     if not first_half or not second_half:
         return False
 
-    if list(reversed(second_half)) == sorted(second_half) and (first_half == sorted(first_half)):
-        return True    
+    # if list(reversed(second_half)) == sorted(second_half) and (first_half == sorted(first_half)):
+    #     return True    
+
+
+    print(first_half)
+    if binSearch(first_half, value) != -1 or binSearch(second_half, value) != -1:
+        return True
+
+
+print(checkBitonic([1, 2, 3 ,4 ,5, 4 ,3], 3))
 
 
 
-print(checkBitonic([1, 2, 3 ,4 ,5, 4 ,3]))
+# finding the highest value by splitting array in half and checking the center value whether it is more than
+# the left element and less than the next right element
+# this should be O(log(N))
+# Then we use binary search to find the value in the increasing part and the descending part, this is O(Log(N))
 
 
+
+
+# Old comments
 # N
 # sorted is log(N) in python
 # all complexity should be log(N) + Nlog(N)
