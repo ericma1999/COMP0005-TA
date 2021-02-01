2e908693d9564031828fcf44917cb957759c02ba
sudoPom42 <zcabofe@ucl.ac.uk>
2021-02-01 13:11:24 +0000

    pancakes!

---
+++ week4/pancakeSort.py
@@ -0,0 +1,27 @@
+pancakes = [3,2,4,1]
+
+def flip(i,pancakes):
+    sub = pancakes[0:i+1]
+    sub.reverse()
+    pancakes[:] = sub + pancakes[i+1:]
+    print(pancakes)
+
+def pancakeSort(pancakes):
+    count = len(pancakes)
+    maxNum = 0
+    while(count > 0):
+        print(count)
+        maxNum = max(pancakes[0:count+1])
+        if maxNum == pancakes[count-1]:
+            count -= 1
+        elif maxNum == pancakes[0]:
+            flip(count, pancakes)
+            count -= 1
+        else:
+            flip(pancakes.index(maxNum), pancakes)
+            flip(count, pancakes)
+            count -= 1
+
+print(pancakes)
+pancakeSort(pancakes)
+#print(pancakes)
