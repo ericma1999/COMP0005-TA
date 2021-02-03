feb0d41201a8f60ca82ad290c7d158c06cd3dc51
sudoPom42 <zcabofe@ucl.ac.uk>
2021-02-03 10:29:35 +0000

    Added enhanced pancake sort

--- week4/pancakeSort.py
+++ week4/pancakeSort.py
@@ -1,27 +1,32 @@
-pancakes = [9,8,7,6,5,4,3,2,1]
+pancakes = [3,2,4,1]
 
 def flip(i,pancakes):
     sub = pancakes[0:i+1]
     sub.reverse()
     pancakes[:] = sub + pancakes[i+1:]
-    print(pancakes)
 
 def pancakeSort(pancakes):
     count = len(pancakes)
     maxNum = 0
+    flippedIndecies = []
     while(count > 0):
-        print(count)
         maxNum = max(pancakes[0:count])
         if maxNum == pancakes[count-1]:
             count -= 1
         elif maxNum == pancakes[0]:
             flip(count-1, pancakes)
             count -= 1
+            flippedIndecies.append(0)
         else:
+            flippedIndecies.append(pancakes.index(maxNum))
             flip(pancakes.index(maxNum), pancakes)
             flip(count-1, pancakes)
+            flippedIndecies.append(0)
             count -= 1
+    return flippedIndecies
 
-print(pancakes)
-pancakeSort(pancakes)
-#print(pancakes)
+def enhancedFlip(i,j,pancakes):
+    sub = pancakes[i:j+1]
+    sub.reverse()
+    pancakes[:] = pancakes[0:i] + sub + pancakes[j+1:]
+    print(pancakes)
